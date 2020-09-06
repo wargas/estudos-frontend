a0cb4e3c3bf105edcb5ad28f16be5a2f42e75454
Wargas Teixeira <wargasteixeira@hotmail.com>
2020-09-06 10:48:24 -0300

    clear

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header: React.FC = props => {
+    return (
+        <React.Fragment>
+            <h1>Header</h1>
+        </React.Fragment>
+    )
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,43 +1,19 @@
-import React, { useRef, useEffect } from 'react';
+import React, {  } from 'react';
 import './Layout.scss';
 import { Sidebar } from '../sidebar/Sidebar';
 import { Routes } from '../Routes';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Tempo } from '../tempo/Tempo';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 
-export const Layout: React.SFC<LayoutProps> = props => {
+export const Layout: React.SFC<LayoutProps> = () => {
 
     
 
     return (
         <BrowserRouter basename="estudos">
-                <div className="wrapper"  >
-                    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <button className="btn">
-                                    <i className="fas fa-bars"></i>
-                                </button>
-                            </li> 
-                        </ul>
-                        {/* <div className="ml-auto">
-                            <Switch>
-                                <Route exact path="/aula/:id" component={Tempo} />
-                            </Switch>
-                        </div> */}
-                    </nav>
-                    <aside className="main-sidebar sidebar-dark-primary elevation-4">
-                        <Sidebar />
-                    </aside>
-
-                    <div className="content-wrapper">
-                        <h1>Layout 2</h1>
-                        <Routes />
-                    </div>
-                </div>
+                <h1>Clear</h1>
             
         </BrowserRouter>
     )
